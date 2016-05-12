c6f21e81cb3522b6991536d66673794e30249e3a
Jacob Pedersen <jacob@bytesizedpacket.com>
2016-05-12 13:17:28 +0200

    Working Mojang auth (needs hardening)

--- src/main/java/com/frash23/onflie/EncryptionResponsePacket.java
+++ src/main/java/com/frash23/onflie/EncryptionResponsePacket.java
@@ -1,17 +1,39 @@
 package com.frash23.onflie;
 
+import com.google.common.base.Preconditions;
+import com.google.common.math.BigIntegerMath;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.EncryptionUtil;
+import net.md_5.bungee.Util;
+import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.connection.InitialHandler;
+import net.md_5.bungee.connection.LoginResult;
+import net.md_5.bungee.http.HttpClient;
+import net.md_5.bungee.jni.cipher.BungeeCipher;
+import net.md_5.bungee.netty.ChannelWrapper;
+import net.md_5.bungee.netty.PipelineUtils;
+import net.md_5.bungee.netty.cipher.CipherDecoder;
+import net.md_5.bungee.netty.cipher.CipherEncoder;
 import net.md_5.bungee.protocol.AbstractPacketHandler;
+import net.md_5.bungee.protocol.packet.EncryptionRequest;
 import net.md_5.bungee.protocol.packet.EncryptionResponse;
+import net.md_5.bungee.protocol.packet.LoginRequest;
 
+import javax.crypto.SecretKey;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
 
 public class EncryptionResponsePacket extends EncryptionResponse {
 
 	static Field requestField;
-	static Field thisStateField;
 	static Field loginProfileField;
+	static Field thisStateField;
+	static Field uniqueIdField;
+	static Field chField;
 	static Method finishMethod;
 	private static boolean accessible = false;
 
@@ -20,6 +42,8 @@ public class EncryptionResponsePacket extends EncryptionResponse {
 			requestField = InitialHandler.class.getDeclaredField("request");
 			loginProfileField = InitialHandler.class.getDeclaredField("loginProfile");
 			thisStateField = InitialHandler.class.getDeclaredField("thisState");
+			uniqueIdField = InitialHandler.class.getDeclaredField("uniqueId");
+			chField = InitialHandler.class.getDeclaredField("ch");
 			finishMethod = InitialHandler.class.getDeclaredMethod("finish");
 		} catch(NoSuchFieldException | NoSuchMethodException e) { /* HANDLE THIS SOMETIME PLEASE */ }
 	}
@@ -28,28 +52,63 @@ public class EncryptionResponsePacket extends EncryptionResponse {
 	@Override
 	public void handle(AbstractPacketHandler handler) throws Exception {
 		if(!accessible) setAccessibility();
-		//requestField.setAccessible(true);
-		//loginProfileField.setAccessible(true);
-		//thisStateField.setAccessible(true);
-		//finishMethod.setAccessible(true);
-		//EncryptionRequest request = (EncryptionRequest)requestField.get(handler);
 
-		//InitialHandler.State.HANDSHAKE
-		//Preconditions.checkState( s)
+		EncryptionRequest request = (EncryptionRequest)requestField.get(handler);
+		ChannelWrapper ch = (ChannelWrapper)chField.get(handler);
+		//Preconditions.checkState( (InitialHandler.State)thisStateField.get(handler), "Not expecting ENCRYPT" );
 
-		//SecretKey sharedKey = EncryptionUtil.getSecret(this, request);
-		//BungeeCipher decrypt = EncryptionUtil.getCipher(false, sharedKey);
+		SecretKey sharedKey = EncryptionUtil.getSecret(this, request);
+		BungeeCipher decrypt = EncryptionUtil.getCipher(false, sharedKey);
+		ch.addBefore( PipelineUtils.FRAME_DECODER, PipelineUtils.DECRYPT_HANDLER, new CipherDecoder(decrypt) );
+		BungeeCipher encrypt = EncryptionUtil.getCipher(true, sharedKey);
+		ch.addBefore( PipelineUtils.FRAME_DECODER, PipelineUtils.ENCRYPT_HANDLER, new CipherEncoder(encrypt) );
 
+		String encName = URLEncoder.encode( ((InitialHandler)handler).getName(), "UTF-8" );
 
-		//finishMethod.invoke(handler);
+		MessageDigest sha = MessageDigest.getInstance("SHA-1");
+		byte[][] bits = new byte[][] {
+				request.getServerId().getBytes("ISO_8859_1"),
+				sharedKey.getEncoded(),
+				EncryptionUtil.keys.getPublic().getEncoded()
+		};
+		for(byte[] bit : bits) sha.update(bit);
+		String encodedHash = URLEncoder.encode( new BigInteger( sha.digest() ).toString(16), "UTF-8" );
 
-		handler.handle( this );
+		String authURL = "https://sessionserver.mojang.com/session/minecraft/hasJoined?username=" + encName + "&serverId=" + encodedHash;
+
+		Callback<String> httpHandler = new Callback<String>() {
+			@Override public void done(String result, Throwable error) {
+
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println(result);
+				System.out.println();
+				System.out.println();
+				System.out.println();
+
+				LoginResult obj = BungeeCord.getInstance().gson.fromJson( result, LoginResult.class );
+				try {
+					loginProfileField.set(handler, obj);
+					uniqueIdField.set( handler, Util.getUUID( obj.getId() ) );
+
+					finishMethod.invoke(handler);
+				} catch(IllegalAccessException | InvocationTargetException e) { /* HANDLE THIS PLEASE */ }
+
+			}
+		};
+
+		HttpClient.get( authURL, ch.getHandle().eventLoop(), httpHandler );
+
+		//handler.handle( this );
 	}
 
 	private void setAccessibility() {
 		requestField.setAccessible(true);
 		loginProfileField.setAccessible(true);
 		thisStateField.setAccessible(true);
+		uniqueIdField.setAccessible(true);
+		chField.setAccessible(true);
 		finishMethod.setAccessible(true);
 
 		accessible = true;
